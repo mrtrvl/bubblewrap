72824d86a079374d5ccac94d8aa497cd7cc7f5e0
mrtrvl <mrt@mrt.ee>
2020-03-08 14:20:55 +0200

    Add build

---
+++ build/precache-manifest.22168e54933f7210372909591166ffb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47295af9eb69fbd9501622db4213fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27db80deffecf61e4d22",
+    "url": "/static/css/main.91a59ee3.chunk.css"
+  },
+  {
+    "revision": "358c1e96e612ce370225",
+    "url": "/static/js/2.0ee36d78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0ee36d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27db80deffecf61e4d22",
+    "url": "/static/js/main.9c2bd08f.chunk.js"
+  },
+  {
+    "revision": "91ea1a7f29f7611a1c9e",
+    "url": "/static/js/runtime-main.5d05d5d3.js"
+  },
+  {
+    "revision": "cbdb9ac4b2bcbc637984174ab5521a74",
+    "url": "/static/media/popped.cbdb9ac4.png"
+  }
+]);
